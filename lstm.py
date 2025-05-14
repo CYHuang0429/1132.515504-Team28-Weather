283401a0dbabe43837868f3eee610983c163464f
CYHuang0429 <103359552+CYHuang0429@users.noreply.github.com>
2025-05-14 22:01:21 +0800

    updated a couple files

--- lstm.py
+++ lstm.py
@@ -11,12 +11,22 @@ warnings.filterwarnings('ignore')
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(f"Using \"{device}\" to train the model.")
 
-weather = pd.read_csv("split_master_EastHsinchu.csv")
+weather = pd.read_csv("Masters/split_master_EastHsinchu.csv")
 weather = weather[["Month", "Date", "Hour", "AirTemperature", "Precipitation", "RelativeHumidity", "StationPressure", "WindSpeed", "WindDirection"]]
 
-# weather = weather.rename(columns={'temp': 'temperature', 'datetime': 'date'})
-# weather['date'] = pd.to_datetime(weather['date'])
-# weather.set_index('date')
-print(weather.head(5))
+weather = weather.rename(columns={'temp': 'AirTemperature', 'datetime': 'Date'})
+weather['Date'] = pd.to_datetime(weather['Date'])
+weather.set_index('Date')
+# print(weather.head(5))
 
+weather.dropna(inplace=True)
+# weather['month'] = weather['date'].dt.month
+# weather_naive = weather[['date', 'temperature']].copy(deep=True)
+# weather_naive['prev_temperature'] = weather_naive['temperature'].shift(1)
+# weather_naive.drop([0], inplace=True)
+# weather_naive['difference'] = weather_naive['temperature'] - weather_naive['prev_temperature']
+# weather_naive['square_error'] = weather_naive['difference'] ** 2
+# weather_naive.head(2)
+
+# square_error = weather_naive['square_error'].mean()
 print(weather.info)
