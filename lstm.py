c22a782110779ac0b362e3b84a13842086da573b
蔡承軒 <tedward087@gmail.com>
2025-05-21 16:08:09 +0800

    Update lstm.py

--- lstm.py
+++ lstm.py
@@ -1,35 +1,3 @@
-# import torch
-# import torch.nn as nn
-# import numpy as np
-# import pandas as pd
-# import matplotlib.pyplot as plt
-# import seaborn as sns
-# from sklearn.preprocessing import StandardScaler
-# import warnings
-# warnings.filterwarnings('ignore')
-
-# device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-# print(f"Using \"{device}\" to train the model.")
-
-# weather = pd.read_csv("Masters/split_master_EastHsinchu.csv")
-# weather = weather[["Month", "Date", "Hour", "AirTemperature", "Precipitation", "RelativeHumidity", "StationPressure", "WindSpeed", "WindDirection"]]
-
-# weather = weather.rename(columns={'temp': 'AirTemperature', 'datetime': 'Date'})
-# weather['Date'] = pd.to_datetime(weather['Date'])
-# weather.set_index('Date')
-# # print(weather.head(5))
-
-# weather.dropna(inplace=True)
-# # weather['month'] = weather['date'].dt.month
-# # weather_naive = weather[['date', 'temperature']].copy(deep=True)
-# # weather_naive['prev_temperature'] = weather_naive['temperature'].shift(1)
-# # weather_naive.drop([0], inplace=True)
-# # weather_naive['difference'] = weather_naive['temperature'] - weather_naive['prev_temperature']
-# # weather_naive['square_error'] = weather_naive['difference'] ** 2
-# # weather_naive.head(2)
-
-# # square_error = weather_naive['square_error'].mean()
-# print(weather.info)
 
 import torch
 import torch.nn as nn
@@ -59,10 +27,12 @@ weather[["AirTemperature", "DewPointTemperature", "Precipitation", "Precipitatio
 
 weather.dropna(inplace=True)
 weather["Precipitation"] = np.log1p(weather["Precipitation"])  # log1p轉換
+weather["RainBinary"] = (weather["Precipitation"] > 0).astype(int)  # 二元化降雨量
 
 # 計算每個特徵的MSE
 # target = ["AirTemperature", "Precipitation", "WindSpeed"]
 target = ["Precipitation"]
+binarrTarget = ["RainBinary"]
 resultList = []
 for col in target:
     temp_df = weather[[col]].copy(deep=True)
@@ -94,21 +64,25 @@ targetScaled_df = pd.DataFrame(targetScaled, columns=target, index=weather.index
 windowSize = 24
 Xall = featureScaled_df.values
 Yall = targetScaled_df.values
+Ybin = weather[binarrTarget].values
 X = []
 Y = []
+Yc = []
 for i in range(len(Xall) - windowSize):
     Xwindow = Xall[i:i + windowSize]
     Ywindow = Yall[i + windowSize]
+    Ycwindow = Ybin[i + windowSize]
     X.append(Xwindow)
     Y.append(Ywindow)
+    Yc.append(Ycwindow)
 X = np.array(X)
 Y = np.array(Y)
-
+Yc = np.array(Yc)
 # 自定義Dataset
 class WeatherDataset(Dataset):
     def __init__(self, X, Y):
         self.X = torch.tensor(X, dtype=torch.float32)
-        self.Y = torch.tensor(Y, dtype=torch.float32)
+        self.Y = torch.tensor(Y, dtype=torch.long)
         
 
     def __len__(self):
@@ -116,22 +90,47 @@ class WeatherDataset(Dataset):
     def __getitem__(self, idx):
         return self.X[idx], self.Y[idx]
 # train_test_split
-XtrainFull, Xtest, YtrainFull, Ytest = train_test_split(X, Y, test_size=0.1, shuffle=False)
-Xtrain, Xval, Ytrain, Yval = train_test_split(XtrainFull, YtrainFull, test_size=0.1111, shuffle=False)
+XtrainFull, Xtest, YtrainFull, Ytest, YctrainFull, Yctest = train_test_split(X, Y, Yc,test_size=0.1, shuffle=False)
+Xtrain, Xval, Ytrain, Yval, Yctrain, Ycval = train_test_split(XtrainFull, YtrainFull, YctrainFull, test_size=0.1111, shuffle=False)
 
 train_dataset = WeatherDataset(Xtrain, Ytrain)
 val_dataset = WeatherDataset(Xval, Yval)
 test_dataset = WeatherDataset(Xtest, Ytest)
 
+train_dataset_c = WeatherDataset(Xtrain, Yctrain)
+val_dataset_c = WeatherDataset(Xval, Ycval)
+test_dataset_c = WeatherDataset(Xtest, Yctest)
+
+train_dataset_reg = WeatherDataset(Xtrain, Ytrain)
+val_dataset_reg = WeatherDataset(Xval, Yval)
+
 batch_size = 64
-train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
-test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+train_loader = DataLoader(train_dataset_c, batch_size=batch_size, shuffle=True)
+val_loader = DataLoader(val_dataset_c, batch_size=batch_size, shuffle=False)
+train_loader_reg = DataLoader(train_dataset_reg, batch_size=batch_size, shuffle=False)
+val_loader_reg = DataLoader(train_dataset_reg, batch_size=batch_size, shuffle=False)
 
 for Xbatch, Ybatch in train_loader:
     print("X_batch shape:",Xbatch.shape)
     print("Y_batch shape:",Ybatch.shape)
     break
+
+class LSTMClassifier(nn.Module):
+    def __init__(self, inputSize, hiddenSize):
+        super(LSTMClassifier, self).__init__()
+        self.lstm = nn.LSTM(inputSize, hiddenSize, num_layers=2, batch_first=True, dropout=0.2)
+        self.linear = nn.Linear(hiddenSize, 1)
+        self.sigmoid = nn.Sigmoid()
+    
+    def forward(self, x):
+        lstmOut, _ = self.lstm(x)
+        out = self.linear(lstmOut[:, -1, :])
+        out = self.sigmoid(out)
+        return out
+    
+
+
+
 
 # LSTM
 class LSTM(nn.Module):
@@ -158,6 +157,10 @@ criterion = nn.MSELoss()
 #優化器
 optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 
+classification_model = LSTMClassifier(inputSize, hiddenSize).to(device)
+criterion_c = nn.BCELoss()
+optimizer_c = torch.optim.Adam(classification_model.parameters(), lr=0.001)
+
 #訓練
 numEpochs = 100
 bestLoss = float('inf')
@@ -166,17 +169,18 @@ wait=0
 trainLosses = []
 validLosses = []
 for epoch in range(numEpochs):
-    model.train()
+    classification_model.train()
     trainLoss = 0.0
     for Xbatch, Ybatch in train_loader:
         Xbatch = Xbatch.to(device)
-        Ybatch = Ybatch.to(device)
+        Ybatch = Ybatch.float().to(device)
 
-        optimizer.zero_grad()
-        Ypred = model(Xbatch)
-        loss = criterion(Ypred, Ybatch)
+        optimizer_c.zero_grad()
+        Ypred = classification_model(Xbatch)
+        loss = criterion_c(Ypred, Ybatch)
         loss.backward()
-        optimizer.step()
+        optimizer_c.step()
+        
 
         trainLoss += loss.item()*Xbatch.size(0)
 
@@ -186,14 +190,14 @@ for epoch in range(numEpochs):
     trainLosses.append(trainLoss)
 
     # validation
-    model.eval()
+    classification_model.eval()
     valLoss = 0.0
     with torch.no_grad():
         for Xbatch, Ybatch in val_loader:
             Xbatch = Xbatch.to(device)
-            Ybatch = Ybatch.to(device)
+            Ybatch = Ybatch.float().to(device)
 
-            Ypred = model(Xbatch)
+            Ypred = classification_model(Xbatch)
             loss = criterion(Ypred, Ybatch)
             valLoss += loss.item() * Xbatch.size(0)
 
@@ -212,10 +216,51 @@ for epoch in range(numEpochs):
             print("Early stopping")
             break
 
+classification_model.load_state_dict(torch.load("best_classifier.pt"))
+classification_model.eval()
 
-
-
+with torch.no_grad():
+    Xtest_tensor = torch.tensor(Xtest, dtype=torch.float32).to(device)
+    rain_probs = classification_model(Xtest_tensor).cpu().numpy().squeeze()
+    rain_preds = (rain_probs >= 0.5).astype(int)
 # test
+rain_indices = np.where(rain_preds == 1)[0]
+
+X_rain = Xtest[rain_indices]
+Y_rain_true = Ytest[rain_indices]
+
+# 測試模型載入
+model.load_state_dict(torch.load("best_model.pt"))
+model.eval()
+with torch.no_grad():
+    X_rain_tensor = torch.tensor(X_rain, dtype=torch.float32).to(device)
+    Y_rain_pred_scaled = model(X_rain_tensor).cpu().numpy()
+
+# 反標準化 + 還原 log1p
+Y_rain_true_real = targetScaler.inverse_transform(Y_rain_true)
+Y_rain_pred_real = targetScaler.inverse_transform(Y_rain_pred_scaled)
+
+Y_rain_true_real[:, 0] = np.expm1(Y_rain_true_real[:, 0])
+Y_rain_pred_real[:, 0] = np.expm1(Y_rain_pred_real[:, 0])
+
+# 評估回歸效果
+
+mae = mean_absolute_error(Y_rain_true_real[:, 0], Y_rain_pred_real[:, 0])
+rmse = np.sqrt(mean_squared_error(Y_rain_true_real[:, 0], Y_rain_pred_real[:, 0]))
+r2 = r2_score(Y_rain_true_real[:, 0], Y_rain_pred_real[:, 0])
+print(f"Precipitation on Rainy Hours → MAE: {mae:.2f}, RMSE: {rmse:.2f}, R²: {r2:.4f}")
+
+
+plt.figure(figsize=(12, 4))
+plt.plot(rain_probs[:200], label="Predicted Probability")
+plt.plot(Yctest[:200], label="Actual RainBinary")
+plt.legend()
+plt.title("Rain Prediction vs Actual")
+plt.grid(True)
+plt.tight_layout()
+plt.show()
+
+'''
 model.eval()
 testLoss = 0.0
 with torch.no_grad():
@@ -263,7 +308,7 @@ plt.grid(True)
 plt.legend()
 plt.tight_layout()
 plt.show()
-
+'''
 
 '''
  模型強化：加入 Dropout / 多層 LSTM / Early Stopping
@@ -280,13 +325,4 @@ plt.show()
 
 
 
-# weather['month'] = weather['date'].dt.month
-# weather_naive = weather[['date', 'temperature']].copy(deep=True)
-# weather_naive['prev_temperature'] = weather_naive['temperature'].shift(1)
-# weather_naive.drop([0], inplace=True)
-# weather_naive['difference'] = weather_naive['temperature'] - weather_naive['prev_temperature']
-# weather_naive['square_error'] = weather_naive['difference'] ** 2
-# weather_naive.head(2)
-
-# square_error = weather_naive['square_error'].mean()
 print(weather.info)
